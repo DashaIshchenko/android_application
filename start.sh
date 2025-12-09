b067edab9e6c4625e3635de9bdef08854436ce4b
DashaIshchenko <dadaskaska@gmail.com>
2025-12-09 22:06:29 +0500

    #9. Перенесла файл в папку
    Удалила дубликат без папки

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-JAVA_CMD=$(which java)
-JAR_FILE="/home/pi/localmesh-chat/LocalMeshChatServer.jar"
-LOG_FILE="/var/log/localmesh.log"
-
-$JAVA_CMD -Xms128m -Xmx256m -jar $JAR_FILE >> $LOG_FILE 2>&1 &
-echo $! > /var/run/localmesh.pid
